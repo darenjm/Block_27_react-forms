9faec71d4d1d4fc4ea9065bb0ce2426d64971485
Daren Morreale <daren@givelegacy.com>
2025-03-01 05:59:26 -0500

    added a littl styel

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -8,39 +8,41 @@ export default function SignUpForm() {
   async function handleSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup"
+      );
       const result = await response.json();
       console.lot(result);
     } catch (error) {
-        setError("Incorrect username or password");
+      setError("Incorrect username or password");
     }
   }
 
-  return (
+return (
     <>
-      <h2>Sign Up</h2>
-      {error && <p>Error: {error}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            name="username"
-            placeholder="Username"
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            name="password"
-            placeholder="Password"
-          />
-        </label>
-        <button>Submit</button>
-      </form>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:{" "}
+                <input
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    name="username"
+                    placeholder="Username"
+                />
+            </label>
+            <label>
+                Password:{" "}
+                <input
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    name="password"
+                    placeholder="Password"
+                />
+            </label>
+            <button>Submit</button>
+        </form>
+        {error && <p style={{ padding: "10px 20px", background: "red", displey: "block", minWidth: "20%", margin: "10px auto", borderRadius: "20px", fontWeight: "900" }}>!!! {error} !!!</p>}
     </>
-  );
+);
 }
